d3d48199ef7f0e3a52df87f15b2e971952a3a384
Unknown <vmrdz220@gmail.com>
2018-07-23 21:48:38 -0500

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashComponent } from './dash/dash.component';
+
+const routes: Routes = [
+  {
+    path:"",
+    component: LoginComponent
+  },
+  {
+    path:"dashboard",
+    component: DashComponent
+  },
+  {
+    path: "",
+    redirectTo: "/",
+    pathMatch: "full"
+  },
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+
