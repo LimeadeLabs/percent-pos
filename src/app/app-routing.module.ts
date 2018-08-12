cab02fe288ea8fdb06cf57ac581d891fbea9bcdc
Victor Rodriguez Jr <vmrdz220@gmail.com>
2018-08-12 15:11:27 -0500

    Added HttpModule import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashComponent } from './dash/dash.component';
 import { RegisterComponent } from './register/register.component';
+import { HttpModule } from '@angular/http';
 
 const routes: Routes = [
   {
@@ -26,7 +27,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+     RouterModule.forRoot(routes), 
+     HttpModule 
+    ],
   exports: [RouterModule]
 })
 
